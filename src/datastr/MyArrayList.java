af00ab1f03db69152475a7d3702036f0d8212b29
Niklāvs Bergs <s21bergnikl@vea.lv>
2023-02-27 09:58:13 +0200

    Added functions

--- src/datastr/MyArrayList.java
+++ src/datastr/MyArrayList.java
@@ -18,6 +18,36 @@ public class MyArrayList {
 	}
 	
 	public boolean isFull() {
-		return elementCounter >= arraySize;
+		return elementCounter == arraySize;
+	}
+	
+	public boolean isEmpty() {
+		return elementCounter == 0;
+	}
+	
+	public int returnCount() {
+		return elementCounter;
+	}
+	
+	public char[] doubleSize() {
+		int newSize;
+		char[] newArray;
+		if (elementCounter > 100) {
+			newSize = (int) (arraySize * 1.5);
+		}
+		else {
+			newSize = arraySize * 2;
+		}
+		newArray = new char[newSize];
+		
+		for (int i = 0; i < elementCounter; i++) {
+			newArray[i] = elements[i];
+		}
+		
+		return newArray;
+	}
+	
+	public char[] addElementEnd{
+		
 	}
 }
