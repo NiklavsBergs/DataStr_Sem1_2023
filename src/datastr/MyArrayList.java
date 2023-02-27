d1dd4bc5d28a66de400a856c5d25ebd24b879981
Niklāvs Bergs <s21bergnikl@vea.lv>
2023-02-27 09:40:44 +0200

    Added variables

--- src/datastr/MyArrayList.java
+++ src/datastr/MyArrayList.java
@@ -1,5 +1,23 @@
 package datastr;
 
 public class MyArrayList {
-
+	private char[] elements;
+	private final int DEFAULT_ARRAY_SIZE = 6;
+	private int arraySize = DEFAULT_ARRAY_SIZE;
+	private int elementCounter = 0;
+	
+	public MyArrayList() {
+		elements = new char[arraySize];
+	}
+	
+	public MyArrayList(int inputArraySize) {
+		if (inputArraySize > 0){
+			arraySize = inputArraySize;
+		}
+		elements = new char[arraySize];
+	}
+	
+	public boolean isFull() {
+		return elementCounter >= arraySize;
+	}
 }
